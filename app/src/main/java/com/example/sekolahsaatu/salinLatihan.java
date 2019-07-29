ea78856b3bff2d18765157ef00a97322435d4b42
redcountry15 <nfrostyy17@gmail.com>
2019-07-29 10:43:58 +0700

    aku ganteng

---
+++ app/src/main/java/com/example/sekolahsaatu/salinLatihan.java
@@ -0,0 +1,14 @@
+package com.example.sekolahsaatu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class salinLatihan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salin_latihan);
+    }
+}
